7dae003b1a2df252b4e283d0a6d60db3800e12da
thanaporn.ch <thanaporn_chan@cmu.ac.th>
2024-11-29 18:25:50 +0700

    #create: index

---
+++ src/pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <title>Queue Management</title>
+        <meta name="description" content="ENGR Queue Management System" />
+        <link rel="icon" href="/icon-web.png" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
