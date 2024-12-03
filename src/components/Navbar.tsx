038778cd14d8a0f3339309d0a810766e856647f3
worapit <drm.wmwork@gmail.com>
2024-12-03 16:43:43 +0700

    Navbar & Set font for TH and ENG

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,32 @@
 import Profile from "./Profile";
+import Image from "next/image";
+import logoEng from "../../public/images/logoEng2.png";
+import { useState } from "react";
 
 export default function Navbar() {
+  const role = "admin";
   return (
     <>
       <div
         className="min-h-14 bg-gradient-to-r from-[#8C0707] to-[#1B0438] border-b border-[#e0e0e0] text-secondary px-6 inline-flex w-full justify-between items-center z-50 border-none"
         style={{ boxShadow: "0px 0px 4px 0px rgba(0, 0, 0, 0.25)" }}
       >
-        <div className="flex w-fit gap-3 items-center">
-          <p className="font-semibold text-h2 md:w-fit w-full">{}</p>
+        <div className="flex items-center gap-2 -ml-1">
+          <Image className=" z-50 w-[45px]" src={logoEng} alt="loginImage" />
+          <div className="flex flex-col w-fit gap-0 font-medium text-b3 text-white">
+            {role === "admin" ? (
+              <p>ระบบจัดการคิว คณะวิศวกรรมศาสตร์ มหาวิทยาลัยเชียงใหม่</p>
+            ) : (
+              <p>ระบบรับบัตรคิว คณะวิศวกรรมศาสตร์ มหาวิทยาลัยเชียงใหม่</p>
+            )}
+            {role === "admin" ? (
+              <p>Queue Management - Engineering CMU</p>
+            ) : (
+              <p>Ticket Queue - Engineering CMU</p>
+            )}
+          </div>
         </div>
+
         <Profile />
       </div>
     </>
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,3 +1,10 @@
+import { Icon } from "lucide-react";
+
 export default function Profile() {
-  return <div></div>;
+  return (
+    <div className="flex flex-col w-fit gap-0 font-medium text-b3 text-white items-end">
+      <p>ประเสริฐ จิระอนันต์</p>
+      <p>ผู้ดูแลระบบ</p>
+    </div>
+  );
 }
